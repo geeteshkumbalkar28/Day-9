146e662d0e334f2dc4cef3d851c3413afafa4a68
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-01-11 03:56:56 +0530

    Added : write function to get workHour

---
+++ FunWOrkHour.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+halfDay=1
+fullDay=2
+perHourSal=100
+monthDay=31
+maxDay=20
+totalHour=0
+preDay=0
+absDay=0
+day=1
+
+while [[ $day -le $monthDay && $preDay -lt $maxDay && $totalHour -lt 100 ]]
+do
+	ranCheck=$((RANDOM%3))
+	case $ranCheck in
+		$halfDay)
+			empHour=4
+			((preDay++)) ;;
+		$fullDay)
+			empHour=8
+			((preDay++));;
+		*)
+			empHour=0
+			((absDay++)) ;;
+	esac
+	totalHour=$(($totalHour+$empHour))
+	((day++))
+done
+monthlySal=$(($totalHour*$perHourSal))
+echo "total monthly salary is $monthlySal"
+echo "TOtal Hour Worked is $totalHour"
